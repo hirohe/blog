b8e64d1262ae2843b86c96d90e02faa99c42717b
hirohe <hesreg@hotmail.com>
2019-05-25 16:18:57 +0800

    add SendButton

---
+++ src/components/Comment/SendButton.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Slide from '@material-ui/core/Slide';
+import Fab from '@material-ui/core/Fab';
+import SendIcon from '@material-ui/icons/Send';
+import { animated, useSpring } from 'react-spring';
+
+
+function SendButton({ canSend, sending, onSend }) {
+  const [state, toggle] = useState(true);
+  const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: { duration: 500 } });
+
+  return (
+    <Slide in={!sending} direction={sending ? 'left' : 'right'} timeout={sending ? 200 : 1000}>
+      <animated.div
+        style={{
+          float: 'right',
+          transform: x
+            .interpolate({
+              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+              output: [1, 1.5, 1.1, 0.95, 1, 1.4, 1.1, 1]
+            })
+            .interpolate(x => `scale(${x})`)
+        }}
+        onClick={() => !canSend && toggle(!state)}
+      >
+        <Fab
+          color="primary"
+          size="small"
+          onClick={onSend}
+        >
+          <SendIcon />
+        </Fab>
+      </animated.div>
+    </Slide>
+  )
+}
+
+export default SendButton;
